373ec274afdf1a3ab42fcd6bb07e700e134ac065
Sabimango <yuifrim@gmail.com>
2021-11-03 11:20:34 +0100

    Javascript update
    a thicc update for the thicc bae

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,14 @@
-var glass1 = document.getElementById("glasses1");
-var glass2 = document.getElementById("glasses2");
-var glass3 = document.getElementById("glasses3");
-var glassPos = [100, 200, 300];
+var glassPos = [0, 100, 200, 300];
+var currentActive = 0;
 
 function nextGlasses() {
-  glassPos[2] = glassPos[1];
-  glassPos[1] = glassPos[0];
-  glassPos[0] -= 100;
-
-  if (glassPos[0] > 100) glassPos[0] = 300;
-
-  for (var i = 0; i <= 2; i++) {
-    document.getElementById("glasses" + (i + 1)).style.left = glassPos[i] + "%";
+  console.log(glassPos[currentActive]);
+  document.getElementById("glasses-variants").style.left =
+    -glassPos[currentActive] + "%";
+  console.log(currentActive, glassPos.length);
+  if (currentActive == glassPos.length - 1) {
+    currentActive = 0;
+  } else {
+    currentActive++;
   }
 }
