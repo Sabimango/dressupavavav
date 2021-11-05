9a2095e561eaeda0e56a402e90b6e8e56d426cd6
Sabimango <yuifrim@gmail.com>
2021-11-05 14:39:44 +0100

    Fixed scripts & styles
    YAAAAAY

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,33 +2,36 @@ let currentActive = 0;
 const glassPos = [0, 100, 200, 300];
 const resultBox = document.getElementById("results");
 const makeGlasses = [
-{
+  {
     imgURL: ["images/0glass1.png", "images/0glass2.png", "images/0glass3.png"],
     charaURL: "images/0frog.png",
     answer: "glasses2",
-},
-{
-    imgURL: ["placeholder"]
-}
+  },
+  {
+    imgURL: ["placeholder"],
+  },
 ];
 
-function displayGlasses(){
-    for (let i = 1; i <= 3; i++ ) {
-        document.getElementById("glasses" + i).style.background = `url(${makeGlasses[0].imgURL[i - 1]})`;
-    }
+function displayGlasses() {
+  for (let i = 1; i <= 3; i++) {
+    document.getElementById("glasses" + i).style.background = `url(${
+      makeGlasses[0].imgURL[i - 1]
+    })`;
+  }
 
-    document.getElementById("character").style.background = `url(${makeGlasses[0].charaURL})`;
+  document.getElementById(
+    "character"
+  ).style.background = `url(${makeGlasses[0].charaURL})`;
 }
 
 function nextGlasses() {
-    
-        for (let i = 1; i <= 3; i++) {
-          document.getElementById("glasses" + i).style.visibility = "hidden";
-        }
-        if (currentActive != 0) {
-        document.getElementById("glasses" + currentActive).style.visibility =
-          "visible";
-      }
+  for (let i = 1; i <= 3; i++) {
+    document.getElementById("glasses" + i).style.visibility = "hidden";
+  }
+  if (currentActive != 0) {
+    document.getElementById("glasses" + currentActive).style.visibility =
+      "visible";
+  }
 
   console.log(glassPos[currentActive]);
   document.getElementById("glasses-variants").style.left =
@@ -38,24 +41,19 @@ function nextGlasses() {
     currentActive = 0;
   } else {
     currentActive++;
-    document.getElementById("glasses-variants").style.transition = "all 2000ms";
+    
   }
-
- 
 }
 
 displayGlasses();
 nextGlasses();
 
 function checkAnswer() {
-    if (rightAnswer.checked) {
-      resultBox.innerHTML = "Correct Answer";
-    } else {
-      resultBox.innerHTML = "Wrong Answer";
-    }
+  console.log("current'active", currentActive);
+
+  if (currentActive === 3) {
+    resultBox.innerHTML = "Correct Answer";
+  } else {
+    resultBox.innerHTML = "Wrong Answer";
   }
-
-
-
-
-  /* rightanswer ska vara en boolean som sätts till true när man klickar på rätta svaret */
+}
